649839384f14c805312584bead7784fe446f9645
Anton Kryukov <anton.a.kryukov@gmail.com>
2021-03-15 15:25:34 +0300

    project item markup

--- lib/presentation/shared/widgets/header_widget.dart
+++ lib/presentation/shared/widgets/header_widget.dart
@@ -18,38 +18,88 @@ class _HeaderWidgetState extends State<HeaderWidget> {
   @override
   Widget build(BuildContext context) {
     UserController controller = Get.put(UserController());
-    return new Container(
-      height: 70,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            width: 72,
-            child: SVG.createSized(
-                'lib/assets/images/icons/project_icon.svg', 40, 40),
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 56,
+                child: SVG.createSized(
+                    'lib/assets/images/icons/project_icon.svg', 40, 40),
+              ),
+              Expanded(
+                child: Text(
+                  'Projects',
+                  style: TextStyleHelper.headerStyle,
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    IconButton(
+                        icon: const Icon(Icons.search), onPressed: () {}),
+                    IconButton(
+                        icon: SVG
+                            .create('lib/assets/images/icons/preferences.svg'),
+                        onPressed: () {}),
+                    IconButton(
+                        icon: const Icon(Icons.more_vert_outlined),
+                        onPressed: () {}),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Text(
-              'Projects',
-              style: TextStyleHelper.headerStyle,
-            ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              // const SizedBox(width: 16),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '3 projects',
+                      style: TextStyleHelper.subtitleProjects,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(right: 4),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Deadline',
+                      style: TextStyleHelper.projectsSorting,
+                    ),
+                    const SizedBox(width: 8),
+                    SVG.createSized(
+                        'lib/assets/images/icons/sorting_3_descend.svg',
+                        20,
+                        20),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                IconButton(icon: const Icon(Icons.search), onPressed: () {}),
-                IconButton(icon: const Icon(Icons.filter), onPressed: () {}),
-                IconButton(
-                    icon: const Icon(Icons.more_vert_outlined),
-                    onPressed: () {}),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+        const Divider(
+          height: 1,
+          thickness: 1,
+          indent: 0,
+          endIndent: 0,
+        ),
+      ],
     );
   }
 }
