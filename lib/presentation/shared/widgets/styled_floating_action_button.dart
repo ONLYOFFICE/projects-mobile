ff6da0a784607c9fd237ef9bd60568e7ab9ce66f
Rakhman Rustamov <letayvisoko@gmail.com>
2021-03-23 18:45:05 +0300

    colors change

--- lib/presentation/shared/widgets/styled_floating_action_button.dart
+++ lib/presentation/shared/widgets/styled_floating_action_button.dart
@@ -22,13 +22,13 @@ class StyledFloatingActionButton extends StatelessWidget {
         shape: BoxShape.circle,
         boxShadow: [
           BoxShadow(
-            color: AppColors.black.withOpacity(0.2),
+            color: AppColors.onSurface.withOpacity(0.2),
             blurRadius: 10,
             spreadRadius: 1,
             offset: Offset(0, 1)
           ),
           BoxShadow(
-            color: AppColors.black.withOpacity(0.12),
+            color: AppColors.onSurface.withOpacity(0.12),
             blurRadius: 5,
             spreadRadius: 1,
             offset: Offset(0, 4)
