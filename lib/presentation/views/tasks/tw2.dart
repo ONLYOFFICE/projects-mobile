d88717f76a6502bfecca11e779b3c51389684199
Rakhman Rustamov <letayvisoko@gmail.com>
2021-03-23 18:46:32 +0300

    task view updates

---
+++ lib/presentation/views/tasks/tw2.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:projects/domain/controllers/tasks_controller.dart';
+import 'package:projects/presentation/shared/app_colors.dart';
+import 'package:projects/presentation/shared/widgets/header_widget.dart';
+import 'package:projects/presentation/shared/widgets/styled_floating_action_button.dart';
+import 'package:projects/presentation/shared/widgets/task_item.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+class TasksView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.put(TasksController());
+
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      floatingActionButton: StyledFloatingActionButton(
+        child: Icon(Icons.add_rounded),
+      ),
+      // appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          HeaderWidget(title: 'Tasks'),
+          Expanded(
+            child: Obx(() => SmartRefresher(
+                enablePullDown: true,
+                enablePullUp: false,
+                controller: controller.refreshController,
+                onRefresh: controller.onRefresh,
+                onLoading: controller.onLoading,
+                child: ListView.builder(
+                  itemBuilder: (c, i) {
+                    return InkWell(
+                      onTap: () async => await Get.toNamed('TaskDetailedView'),
+                      child: TaskItem(item: controller.tasks[i])
+                    );
+                  },
+                  itemExtent: 100.0,
+                  itemCount: controller.tasks.length,
+                ),
+              )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
