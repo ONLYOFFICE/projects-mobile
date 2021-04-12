a37a300ab4bdedbcb7166097b4a4e005bc9c2149
Anton Kryukov <anton.kryukov@onlyoffice.com>
2021-04-12 19:00:45 +0300

    fix

--- lib/presentation/views/project_detailed/project_detailed_view.dart
+++ lib/presentation/views/project_detailed/project_detailed_view.dart
@@ -83,7 +83,7 @@ class ProjectDetailedView extends StatelessWidget {
                       .customColors()
                       .onSurface
                       .withOpacity(0.6),
-                  labelStyle: TextStyleHelper.subtitle2,
+                  labelStyle: TextStyleHelper.subtitle2(),
                   tabs: [
                     Tab(text: 'Overview'),
                     Tab(text: 'Tasks'),
