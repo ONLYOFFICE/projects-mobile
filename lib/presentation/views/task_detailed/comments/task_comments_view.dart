df8c5b8a9fa23058a55e52d66e306796e52ca86d
Рахман <55310365+fewva@users.noreply.github.com>
2021-04-12 11:48:57 +0300

    Bug fix

--- lib/presentation/views/task_detailed/comments/task_comments_view.dart
+++ lib/presentation/views/task_detailed/comments/task_comments_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
+
 import 'package:get/get.dart';
 import 'package:projects/data/api/core_api.dart';
 import 'package:projects/data/models/from_api/portal_comment.dart';
