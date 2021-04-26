45aac8105d716b721499e50d995e3e7adc8c5726
Rakhman Rustamov <rakhman.rustamov@onlyoffice.com>
2021-04-27 00:35:18 +0300

    bug fix

--- lib/domain/controllers/tasks/new_task_controller.dart
+++ lib/domain/controllers/tasks/new_task_controller.dart
@@ -252,12 +252,15 @@ class NewTaskController extends GetxController {
 
       print(newTask.toJson());
       var createdTask = await _api.addTask(newTask: newTask);
-      var tasksController = Get.find<TasksController>();
-      // ignore: unawaited_futures
-      tasksController.reloadTasks();
-      Get.back();
-      ScaffoldMessenger.of(context)
-          .showSnackBar(_snackBar(context, createdTask));
+      print('createdTask $createdTask');
+      if (createdTask != null) {
+        var tasksController = Get.find<TasksController>();
+        // ignore: unawaited_futures
+        tasksController.loadTasks();
+        Get.back();
+        ScaffoldMessenger.of(context)
+            .showSnackBar(_snackBar(context, createdTask));
+      }
     }
   }
 
