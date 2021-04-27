b2061190d9fbc203ba0ff5b51cb56820d6569711
Rakhman Rustamov <rakhman.rustamov@onlyoffice.com>
2021-04-27 13:05:14 +0300

    task overview matches the layout

--- lib/presentation/shared/widgets/info_tile.dart
+++ lib/presentation/shared/widgets/info_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projects/presentation/shared/theme/custom_theme.dart';
 import 'package:projects/presentation/shared/theme/text_styles.dart';
 
 class InfoTile extends StatelessWidget {
@@ -7,6 +8,8 @@ class InfoTile extends StatelessWidget {
   final String subtitle;
   final TextStyle captionStyle;
   final TextStyle subtitleStyle;
+  final Widget subtitleWidget;
+  final Widget suffix;
 
   const InfoTile({
     Key key,
@@ -15,29 +18,32 @@ class InfoTile extends StatelessWidget {
     this.captionStyle,
     this.subtitle,
     this.subtitleStyle,
+    this.subtitleWidget,
+    this.suffix,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          SizedBox(
-            width: 56,
-            child: icon,
-          ),
-          Flexible(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+    return Row(
+      children: [
+        SizedBox(width: 56, child: icon),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (caption != null)
                 Text(caption, style: captionStyle ?? TextStyleHelper.caption()),
+              if (subtitleWidget != null) subtitleWidget,
+              if (subtitleWidget == null && subtitle != null)
                 Text(subtitle,
-                    style: subtitleStyle ?? TextStyleHelper.subtitle1())
-              ],
-            ),
-          )
-        ],
-      ),
+                    style: subtitleStyle ??
+                        TextStyleHelper.subtitle1(
+                            color: Theme.of(context).customColors().onSurface))
+            ],
+          ),
+        ),
+        if (suffix != null) suffix
+      ],
     );
   }
 }
