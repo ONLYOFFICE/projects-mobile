9d3558f2d54ab78c66ecc5e3de4f83f137b6af16
Rakhman Rustamov <rakhman.rustamov@onlyoffice.com>
2021-07-26 20:44:58 +0300

    fixed FAB color

--- lib/presentation/shared/widgets/styled_floating_action_button.dart
+++ lib/presentation/shared/widgets/styled_floating_action_button.dart
@@ -19,15 +19,15 @@ class StyledFloatingActionButton extends StatelessWidget {
     return DecoratedBox(
       decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
         BoxShadow(
-            color: Get.theme.colors().onSurface.withOpacity(0.2),
+            color: Colors.black.withOpacity(0.2),
             blurRadius: 10,
             spreadRadius: 1,
             offset: const Offset(0, 1)),
-        BoxShadow(
-            color: Get.theme.colors().onSurface.withOpacity(0.12),
+        const BoxShadow(
+            color: Colors.black12,
             blurRadius: 5,
             spreadRadius: 1,
-            offset: const Offset(0, 4)),
+            offset: Offset(0, 4)),
       ]),
       child: FloatingActionButton(
         backgroundColor: Get.theme.colors().lightSecondary,
