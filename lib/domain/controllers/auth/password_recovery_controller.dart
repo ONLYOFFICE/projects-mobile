b47da6ab7c8fe2db367e51932714d0dba0e592a9
Rakhman Rustamov <rakhman.rustamov@onlyoffice.com>
2021-09-02 21:37:01 +0300

    Fixed bug #52319 - password recovery screen

--- lib/domain/controllers/auth/password_recovery_controller.dart
+++ lib/domain/controllers/auth/password_recovery_controller.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:projects/data/services/authentication_service.dart';
-import 'package:projects/domain/controllers/navigation_controller.dart';
 import 'package:projects/internal/locator.dart';
 import 'package:projects/presentation/views/authentication/password_recovery/password_recovery_screen2.dart';
 
@@ -25,9 +24,7 @@ class PasswordRecoveryController extends GetxController {
   Future onConfirmPressed() async {
     if (_checkEmail()) {
       var result = await _authService.passwordRecovery(_emailController.text);
-      if (result != null) {
-        Get.find<NavigationController>().to(const PasswordRecoveryScreen2());
-      }
+      if (result != null) await Get.to(() => const PasswordRecoveryScreen2());
     }
   }
 
