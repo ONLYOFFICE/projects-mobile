26db5ed2e739773220ecbab0a83ec1ca4337f764
Alexander Yuzhin <alexander.yuzhin@onlyoffice.com>
2021-09-07 15:34:00 +0300

    Using constants module

--- lib/presentation/views/authentication/code_views/get_code_views.dart
+++ lib/presentation/views/authentication/code_views/get_code_views.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:projects/domain/controllers/auth/login_controller.dart';
+import 'package:projects/internal/constants.dart';
 import 'package:projects/presentation/shared/theme/custom_theme.dart';
 import 'package:projects/presentation/shared/theme/text_styles.dart';
 import 'package:projects/presentation/shared/widgets/app_icons.dart';
