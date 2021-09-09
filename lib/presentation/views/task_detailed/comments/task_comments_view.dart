46d9b6be9390d76b3b32b691abe8e87e7685840c
Rakhman Rustamov <rakhman.rustamov@onlyoffice.com>
2021-09-09 19:12:42 +0300

    Fixed Bug #51883 - added empty screens for comments, discussions and subtasks

--- lib/presentation/views/task_detailed/comments/task_comments_view.dart
+++ lib/presentation/views/task_detailed/comments/task_comments_view.dart
@@ -1,10 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:projects/domain/controllers/comments/new_comment/new_task_comment_controller.dart';
 import 'package:projects/domain/controllers/navigation_controller.dart';
 import 'package:projects/domain/controllers/tasks/task_item_controller.dart';
 import 'package:projects/presentation/shared/widgets/add_comment_button.dart';
+import 'package:projects/presentation/shared/widgets/app_icons.dart';
 import 'package:projects/presentation/shared/widgets/list_loading_skeleton.dart';
+import 'package:projects/presentation/shared/widgets/nothing_found.dart';
 import 'package:projects/presentation/views/task_detailed/comments/comments_thread.dart';
 import 'package:projects/presentation/views/task_detailed/comments/new_comment_view.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
@@ -25,27 +28,35 @@ class TaskCommentsView extends StatelessWidget {
           var comments = controller.task.value.comments;
           return Column(
             children: [
-              Expanded(
-                child: SmartRefresher(
-                  controller: controller.refreshController,
-                  onRefresh: () async =>
-                      await controller.reloadTask(showLoading: true),
-                  child: ListView.separated(
-                    itemCount: comments.length,
-                    controller: controller.commentsListController,
-                    padding: const EdgeInsets.only(top: 32, bottom: 40),
-                    separatorBuilder: (BuildContext context, int index) {
-                      return const SizedBox(height: 21);
-                    },
-                    itemBuilder: (BuildContext context, int index) {
-                      return CommentsThread(
-                        comment: comments[index],
-                        taskId: controller.task.value.id,
-                      );
-                    },
+              if (comments.isEmpty)
+                Expanded(
+                  child: EmptyScreen(
+                    icon: AppIcon(icon: SvgIcons.comments_not_created),
+                    text: tr('noCommentsCreated'),
                   ),
                 ),
-              ),
+              if (comments.isNotEmpty)
+                Expanded(
+                  child: SmartRefresher(
+                    controller: controller.refreshController,
+                    onRefresh: () async =>
+                        await controller.reloadTask(showLoading: true),
+                    child: ListView.separated(
+                      itemCount: comments.length,
+                      controller: controller.commentsListController,
+                      padding: const EdgeInsets.only(top: 32, bottom: 40),
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(height: 21);
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return CommentsThread(
+                          comment: comments[index],
+                          taskId: controller.task.value.id,
+                        );
+                      },
+                    ),
+                  ),
+                ),
               if (controller?.task?.value?.canCreateComment == null ||
                   controller?.task?.value?.canCreateComment == true)
                 AddCommentButton(
