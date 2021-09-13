a4ee96fdf7f4015d1c1394118c2ea5ddd185c72a
Anton Kryukov <anton.kryukov@onlyoffice.com>
2021-09-13 20:33:01 +0300

    Fixed Bug #51383

--- lib/internal/utils/debug_print.dart
+++ lib/internal/utils/debug_print.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/foundation.dart';
 
-// delete TODOs when you see them
-
-// TODO yellow letters
 void printWarning(var data) => debugPrint('\x1B[33m$data\x1B[0m');
 
-// TODO red letters
 void printError(var data) => print('\x1B[31m$data\x1B[0m');
